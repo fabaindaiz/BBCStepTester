cf768c4b55c28143c0c22b0e2ad9f215f9f90d8d
fadiaz <fabiandiaz@ug.uchile.cl>
2024-11-29 20:11:39 +0000

    feat capture stdout from oracle

--- dev/util.ml
+++ dev/util.ml
@@ -47,3 +47,51 @@ let write_file error file string =
 let read_file error file =
   try Ok (process_string @@ (CCIO.with_in file CCIO.read_all))
   with e -> Error (error, Printexc.to_string e)
+
+
+let has_pending_data fd =
+  try
+    let ready_fds, _, _ = Unix.select [fd] [] [] 0.0 in
+    List.length ready_fds > 0
+  with _ -> false
+
+let capture_stdout_until_done (f : unit -> 'a) : string * 'a =
+  let stdout_original = Unix.dup Unix.stdout in
+  let (pipe_read, pipe_write) = Unix.pipe () in
+  
+  (* pipe no bloqueante para lecturas seguras *)
+  Unix.set_nonblock pipe_read;
+  Unix.dup2 pipe_write Unix.stdout;
+  Unix.close pipe_write;
+  
+  let buffer = Buffer.create 4096 in
+  let string_buffer = Bytes.create 4096 in
+  
+  let result = 
+    try 
+      let r = f () in
+      flush stdout;
+      
+      let rec read_remaining () =
+        if has_pending_data pipe_read then
+          match Unix.read pipe_read string_buffer 0 4096 with
+          | 0 -> ()  (* EOF *)
+          | bytes_read -> 
+              Buffer.add_subbytes buffer string_buffer 0 bytes_read;
+              read_remaining ()
+          | exception Unix.Unix_error(Unix.EAGAIN, _, _) -> 
+              Unix.sleepf 0.001;  (* Pequeña pausa si el pipe está temporalmente vacío *)
+              if has_pending_data pipe_read then read_remaining ()
+      in
+      read_remaining ();
+      Ok r
+    with e -> Error e
+  in
+  
+  Unix.dup2 stdout_original Unix.stdout;
+  Unix.close stdout_original;
+  Unix.close pipe_read;
+  
+  match result with
+  | Ok v -> (Buffer.contents buffer, v)
+  | Error e -> raise e
